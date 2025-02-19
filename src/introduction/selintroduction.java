f190fac083e3d8e3b930828c0d13c4e7da21c139
Shefali <bhatnagarshefali03@gmail.com>
2025-02-19 13:04:02 +0530

    second commit

---
+++ src/introduction/selintroduction.java
@@ -0,0 +1,21 @@
+package introduction;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+public class selintroduction {
+	public static void main(String[] args) {
+		//ChromeDriver driver=new ChromeDriver();
+		WebDriver driver=new ChromeDriver();
+//		driver.get("https://mvnrepository.com/artifact/org.testng/testng/7.11.0");
+//		System.out.println(driver.getTitle());
+//		System.out.println(driver.getCurrentUrl());
+//		driver.close();
+//		driver.quit();
+//		driver.findElement(By.name().sendKeys)
+		driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
+		Select dropdown=new Select();
+	}
+	
+
+}
